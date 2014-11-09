66ce7c3716751216a6199452494e12cd1be6dbbb
Katelyn Slater <ktaeyln@gmail.com>
2014-11-09 15:12:36 -0500

    Append url on redirect, unless stripURL is set

--- handlers/redirect.js
+++ handlers/redirect.js
@@ -1,18 +1,37 @@
+var endSlash = /\/$/;
 module.exports = function(app, config, logger) {
     if(!config.to)
         throw new Error("Handler `redirect` requires `to` parameter");
     
     var redirect;
-    if(config.code)
-        redirect = function(req, res) {
-            logger.info("Redirecting to", config.to, config.code);
-            res.redirect(config.code, config.to);
-        };
-    else
-        redirect = function(req, res) {
-            logger.info("Redirecting to", config.to);
-            res.redirect(config.to);
-        };
+	if(config.stripURL) {
+		if(config.code)
+			redirect = function(req, res) {
+				logger.info("Redirecting to", config.to, config.code);
+				res.redirect(config.code, config.to);
+			};
+		else
+			redirect = function(req, res) {
+				logger.info("Redirecting to", config.to);
+				res.redirect(config.to);
+			};
+	} else {
+		if(endSlash.test(config.to))
+			config.to = config.to.substring(0, config.to.length-1);
+		
+		if(config.code)
+			redirect = function(req, res) {
+				var to = config.to + res.url;
+				logger.info("Redirecting to", to, config.code);
+				res.redirect(config.code, to);
+			};
+		else
+			redirect = function(req, res) {
+				var to = config.to + res.url;
+				logger.info("Redirecting to", to);
+				res.redirect(to);
+			};
+	}
     
     if(config.path)
         app.use(config.path, redirect);
