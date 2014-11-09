a8d1bdbaf93d8ef37c8a4f157998d8d922bc008b
Katelyn Slater <ktaeyln@gmail.com>
2014-11-09 00:37:28 -0500

    Resolve files when missing .html automatically Remove index.html from url automatically

--- handlers/static.js
+++ handlers/static.js
@@ -4,6 +4,35 @@ module.exports = function(app, config) {
     if(!config.root)
         throw new Error("Handler `static` requires `root` parameter");
     
+    if(!config.noRewrite) {
+        var fs = require("fs");
+        app.use(function(req, res, next) {
+            var subpath = req.path;
+            var pos = subpath.indexOf("?");
+            if(pos != -1)
+                subpath = subpath.substring(0, pos);
+
+            if(/index\.html$/.test(subpath)) {
+                  pos = subpath.lastIndexOf("/");
+                if(pos > 0)
+                  subpath = subpath.substring(0, pos);
+                res.redirect(301, subpath.substring(0, pos+1));
+              }
+
+          if (subpath.indexOf('.') === -1 && subpath) {
+              subpath += ".html";
+            var file = path.resolve(config.root, subpath.substring(1));
+            fs.exists(file, function(exists) {
+              if (exists)
+                    res.redirect(subpath);
+              else
+                next();
+            });
+          } else
+            next();
+        });
+    }
+    
     var handler = express.static(config.root);
     if(config.path)
         app.use(config.path, handler);
